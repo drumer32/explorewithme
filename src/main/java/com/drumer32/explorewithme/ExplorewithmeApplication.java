fff66deb7b16821250e50c0da3ba394a14d5ee0e
drumer32 <hikari576@gmail.com>
2022-09-06 17:06:32 +0300

    Initial commit

---
+++ src/main/java/com/drumer32/explorewithme/ExplorewithmeApplication.java
@@ -0,0 +1,13 @@
+package com.drumer32.explorewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorewithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorewithmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drumer32/explorewithme/ExplorewithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drumer32.explorewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorewithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
